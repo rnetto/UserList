d7af997ff82813f06032d369db48e7829ecaeaaf
Renato Netto <renato.silva@delage.com.br>
2022-07-14 13:50:06 -0300

    Adicionar arquivos de projeto.

---
+++ UserList/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserList.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
